4598e7a5d921602166cc14c98b0fc60bcc626fd4
Proteas <proteas.wang@gmail.com>
2012-08-09 00:42:48 +0800

    tags

--- emacs_cfg/development.el
+++ emacs_cfg/development.el
@@ -18,11 +18,78 @@
 
 (semantic-load-enable-semantic-debugging-helpers)
 
-(defun my-c-mode-cedet-hook ()
+(defun proteas-c-mode-cedet-hook ()
   (local-set-key "." 'semantic-complete-self-insert)
   (local-set-key ">" 'semantic-complete-self-insert))
-(add-hook 'c-mode-common-hook 'my-c-mode-cedet-hook)
-(add-hook 'c++-mode-common-hook 'my-c-mode-cedet-hook)
+
+(add-hook 'c-mode-common-hook 'proteas-c-mode-cedet-hook)
+(add-hook 'c++-mode-common-hook 'proteas-c-mode-cedet-hook)
+
+(global-set-key [f10] 'semantic-ia-fast-jump)
+(global-set-key [S-f10]
+				(lambda ()
+				  (interactive)
+				  (if (ring-empty-p (oref semantic-mru-bookmark-ring ring))
+					  (error "Semantic Bookmark ring is currently empty"))
+				  (let* ((ring (oref semantic-mru-bookmark-ring ring))
+						 (alist (semantic-mrub-ring-to-assoc-list ring))
+						 (first (cdr (car alist))))
+					(if (semantic-equivalent-tag-p (oref first tag)
+												   (semantic-current-tag))
+						(setq first (cdr (car (cdr alist)))))
+					(semantic-mrub-switch-tags first))))
+
+(require 'cc-mode)
+(define-key c-mode-base-map [M-S-f10] 'semantic-analyze-proto-impl-toggle)
+(define-key c-mode-base-map (kbd "M-/") 'semantic-ia-complete-symbol-menu)
+
+(require 'eassist nil 'noerror)
+(define-key c-mode-base-map [M-f12] 'eassist-switch-h-cpp)
+(setq eassist-header-switches
+      '(("h" . ("cpp" "cxx" "c++" "CC" "cc" "C" "c" "mm" "m"))
+        ("hh" . ("cc" "CC" "cpp" "cxx" "c++" "C"))
+        ("hpp" . ("cpp" "cxx" "c++" "cc" "CC" "C"))
+        ("hxx" . ("cxx" "cpp" "c++" "cc" "CC" "C"))
+        ("h++" . ("c++" "cpp" "cxx" "cc" "CC" "C"))
+        ("H" . ("C" "CC" "cc" "cpp" "cxx" "c++" "mm" "m"))
+        ("HH" . ("CC" "cc" "C" "cpp" "cxx" "c++"))
+        ("cpp" . ("hpp" "hxx" "h++" "HH" "hh" "H" "h"))
+        ("cxx" . ("hxx" "hpp" "h++" "HH" "hh" "H" "h"))
+        ("c++" . ("h++" "hpp" "hxx" "HH" "hh" "H" "h"))
+        ("CC" . ("HH" "hh" "hpp" "hxx" "h++" "H" "h"))
+        ("cc" . ("hh" "HH" "hpp" "hxx" "h++" "H" "h"))
+        ("C" . ("hpp" "hxx" "h++" "HH" "hh" "H" "h"))
+        ("c" . ("h"))
+        ("m" . ("h"))
+        ("mm" . ("h"))))
+
+(defun proteas-indent-or-complete ()
+   (interactive)
+   (if (looking-at "\\>")
+      (hippie-expand nil)
+      (indent-for-tab-command))
+ )
+
+(global-set-key [(control tab)] 'proteas-indent-or-complete)
+(autoload 'senator-try-expand-semantic "senator")
+(setq hippie-expand-try-functions-list
+      '(
+        senator-try-expand-semantic
+        try-expand-dabbrev
+        try-expand-dabbrev-visible
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-expand-list
+        try-expand-list-all-buffers
+        try-expand-line
+        try-expand-line-all-buffers
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-whole-kill
+        )
+)
+(define-key c-mode-base-map [(tab)] 'proteas-indent-or-complete)
+(define-key c-mode-base-map [(meta ?/)] 'semantic-ia-complete-symbol-menu)
 ;----------------------------------------------------------;
 
 ;; ecb
